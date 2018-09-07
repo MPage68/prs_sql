a5f7af70b004efa8a8a3738fa7482bc6a0fed6e6
mpage68 <mikepage68@live.com>
2018-09-06 23:21:12 -0400

    update

--- PRS capstone scipt.sql
+++ PRS capstone scipt.sql
@@ -142,7 +142,8 @@ INSERT INTO product(`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPat
 
 -- INSERT INTO x VALUES
 -- ();
-
+CREATE USER prs_user@localhost IDENTIFIED BY 'sesame';
+GRANT SELECT, INSERT, DELETE, UPDATE ON prs.* TO prs_user@localhost;
 -- create a user and grant privileges to that user
 -- GRANT SELECT, INSERT, DELETE, UPDATE
 -- ON mma.*
